b4132980de0afae9ceb41306e19df88441980f2e
DeAsia7 <asiamallory277@gmail.com>
2025-03-17 19:57:06 -0400

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,3 @@ import ReactDOM from 'reactdom/client';
 import App from '../App' 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
