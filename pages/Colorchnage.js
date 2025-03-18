a2bfdf8b0d78e864acbb8b1378465f87f45631bf
DeAsia7 <asiamallory277@gmail.com>
2025-03-17 20:51:16 -0400

    updsate

--- pages/Colorchnage.js
+++ pages/Colorchnage.js
@@ -1,3 +1,21 @@
 import React, { useState } from "react";
 
-//function Colorchange ();
+function Colorchange (){
+const [color, setColor] = useState("red");
+const [isClicked, setIsClicked] = useState(false);
+
+useEffect(() => {
+    const interval = setInterval(() => {
+        setColor((previousColor) =. (previousColor === "red" ? "blue" : "red"));
+    } , 1000);
+    },[])
+
+    useeffect9(() => {
+        setColor((previousColor) => (previousColor === "red" ? "blue" : "red"));
+    }
+);
+    }, [isClicked];
+
+}
+)
+}
