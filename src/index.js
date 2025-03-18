5df31787f833d885a692c4e1d8589bd98acc6fa8
DeAsia7 <asiamallory277@gmail.com>
2025-03-17 20:07:38 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'reactdom/client';
-import App from '../App' 
+import App from './App' 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
 
