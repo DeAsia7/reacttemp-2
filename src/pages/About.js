c3a00d2b2ffe0f28f5f0d1c78e4cf020dbad6a10
DeAsia7 <asiamallory277@gmail.com>
2025-04-02 20:51:59 -0400

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ function About() {
     return (
     <div>
         <h1>About us</h1>
-        <p>holy molly donut shop</p>
+        <p>holy molly donut shop has been around for generations and we're proud to say its made from scratch. Every morning a new batch is made and if we have any left over by the end of the day, we will try our best to hand them to someone in need.  </p>
         <p>donuts make everyone happy and its our goal to keep you happy with every bite.</p>
 
 <button onClick={() => setShowMore(!showMore)} > { showMore ? "Show Less" : "Show More"}</button>
