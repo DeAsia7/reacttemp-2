fe9529ee49d780ff46eaaa4a4706825223a742cd
DeAsia7 <asiamallory277@gmail.com>
2025-04-08 16:10:46 -0400

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,21 @@ import {Link} from 'react-router-dom';
 
 function Navbar(){
 return (
+    <div className= "flex justify-center items-center bg-gray-100">
     <nav style={styles.navbar}>
         <ul style={styles.navlist}>
-        <li> <Link to="Home" style={styles.link}> Home </Link></li>
+        <li> <Link to="Home" style={styles.link}> <span className= 'font-semibold text-red-5000 italic'> Home </span></Link></li>
         <li> <Link to="./About" style={styles.link}> About Us</Link></li>
         <li> <Link to="./Contact" style={styles.link}> Contact Us</Link></li>
         <li> <Link to="./Counter" style={styles.link}> Counter</Link></li>
+        <li> <link to="/Colorchange" style={styles.link}> Colorchange</link></li>
         <li> <Link to="./Calculator" style={styles.link}> Calculator</Link></li>
+        
         </ul>
+        { user && <button onClick={handleLogout}>Logout</button> } 
      
     </nav>
+    </div>
 );
 
 }
