00a5a71042ea6c338e528fa0abf268baa5d4d3f3
DeAsia7 <asiamallory277@gmail.com>
2025-04-15 21:03:37 -0400

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React, { useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
-import {Menu, X} from 'lucide-react';
-
+import {ArrowDownToLine, X} from 'lucide-react';
 
 
 function Navbar({user, setUser}) {
     const Navigate = useNavigate();
-    const [menuOpen, setMenuOpen] = useState(false);
+    const [ArrowDownToLine, setArrowDownToLine] = useState(false);
 
 
     const handleLogout = () => {
@@ -15,7 +14,7 @@ function Navbar({user, setUser}) {
         Navigate('/login');
     };
 
-    const toggleMenu = () => setMenuOpen(!menuOpen);
+    const toggleArrowDownToLine = () => setArrowDownToLine(!ArrowDownToLineOpen);
 
 return (
    // <header className="w-full bg-white shadow-md sticky top-o z-50"
@@ -26,25 +25,25 @@ return (
 
 <div className="md:hidden">
     <button onClick={toggleMenu}>
-        {menuOpen ? <X className="w-6 h-6"/> : <Menu className=" w-6 h-6"/>}
+        {ArrowDownToLine ? <X className="w-6 h-6"/> : <ArrowDownToLine className=" w-6 h-6"/>}
     </button>
 </div>
         <ul className={`md: flex gap-6 items-center text-gray-700 text-lg font-medium list-none p-0 m-0
-         ${menuOpen ? 'block absolute top-[64px] left-0 w-full bg-white px-6 py-4 shadow-md' : "hidden md:flex"}
+         ${ArrowDownToLine ? 'block absolute top-[64px] left-0 w-full bg-white px-6 py-4 shadow-md' : "hidden md:flex"}
         }`}>
-        <li> <Link to="./Home"  className="text-blue-500" onClick={() => setMenuOpen(false)}> Home </Link></li>
+        <li> <Link to="./Home"  className="text-blue-500" onClick={() => setArrowDownToLineOpen(false)}> Home </Link></li>
 
-        <li> <Link to="./About" className="text-yellow-500 " onClick={() => setMenuOpen(false)}> About Us</Link></li>
+        <li> <Link to="./About" className="text-yellow-500 " onClick={() => setArrowDownToLineOpen(false)}> About Us</Link></li>
 
-        <li> <Link to="./Contact"  className="text-pink-600 " onClick={() => setMenuOpen(false)}> Contact Us</Link></li>
+        <li> <Link to="./Contact"  className="text-pink-600 " onClick={() => setArrowDownToLineOpen(false)}> Contact Us</Link></li>
 
-        <li> <Link to="./Counter"  className=" text-red-500 " onClick={() => setMenuOpen(false)}> Counter</Link></li>
+        <li> <Link to="./Counter"  className=" text-red-500 " onClick={() => setArrowDownToLineOpen(false)}> Counter</Link></li>
 
-        <li> <Link to="./Colorchange"  className=" text-indigo-500 " onClick={() => setMenuOpen(false)}> Colorchange</Link></li>
+        <li> <Link to="./Colorchange"  className=" text-indigo-500 " onClick={() => setArrowDownToLineOpen(false)}> Colorchange</Link></li>
 
-        <li> <Link to="./Calculator"  className=" text-gray-500 " onClick={() => setMenuOpen(false)}> Calculator</Link></li>
+        <li> <Link to="./Calculator"  className=" text-gray-500 " onClick={() => setArrowDownToLineOpen(false)}> Calculator</Link></li>
 
-        <li><Link to="./login" className="text-grey-500" onClick={() => setMenuOpen(false)} >logout</Link></li>
+        <li><Link to="./login" className="text-grey-500" onClick={() => setArrowDownToLine(false)} >logout</Link></li>
 
 
 
@@ -52,7 +51,7 @@ return (
         
         </ul>
         { user && (
-        <button onClick={() => {handleLogout(); setMenuOpen(false);}} >Logout</button> 
+        <button onClick={() => {handleLogout(); setArrowDownToLineOpen(false);}} >Logout</button> 
         )}
      
     </nav>
