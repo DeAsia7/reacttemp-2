3703702b8eea9ffb4caa7a45b0cee5691e383ffc
DeAsia7 <asiamallory277@gmail.com>
2025-04-22 19:59:24 -0400

    final

--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -9,10 +9,25 @@ function Location() {
     <div>
      
       <div className="bg-blue-300 min-h-screen">
-        <h1 className="text-center text-white text-3xl">We have many locations in Whoville! </h1>
+        <h1 className="text-center text-white text-2xl">We have many locations in Whoville! </h1>
+        <br></br>
 
-        <p className="text-3xl ">location 1: 295 </p>
-        
+        <p className="text-3xl ">location 1: 295 reindeer road, pleasant, whoville 3409 </p>
+        <br></br>
+
+        <p className="text-3xl"> location 2: 6970 Cool St, Hobbit Hole, whoville 3409 </p>
+        <br></br>
+
+        <p className="text-3xl"> location 3: 1234 North Pole, Whoville 3409 </p>
+        <br></br>
+
+        <p className="text-3xl"> location 4: 5678 Candy Cane Lane, Whoville 3409 </p>
+        <br></br>
+
+        <p className="text-3xl"> location 5: 9101 Gingerbread Ave, Whoville 3409 </p>
+        <br></br>
+
+
       </div>
     </div>
   );
