9fe41721f6b96234e0440ce2585c46b73204dc95
DeAsia7 <asiamallory277@gmail.com>
2025-04-22 19:50:05 -0400

    final

--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -10,6 +10,8 @@ function Location() {
      
       <div className="bg-blue-300 min-h-screen">
         <h1 className="text-center text-white text-3xl">We have many locations in Whoville! </h1>
+
+        <p className="text-3xl ">location 1: 295 </p>
         
       </div>
     </div>
