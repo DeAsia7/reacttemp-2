ecfacc2af7ac6032ec5d94098a5e6bafc81e53a3
DeAsia7 <asiamallory277@gmail.com>
2025-04-22 20:15:12 -0400

    final

--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -8,25 +8,27 @@ function Location() {
   return (
     <div>
 
-      
+
      
       <div className="bg-blue-300 min-h-screen">
+        <br></br>
+
         <h1 className="text-center text-white text-2xl">We have many locations in Whoville! </h1>
         <br></br>
 
-        <p className="text-3xl text-green-200">location 1: 295 reindeer road, pleasant, whoville 3409 </p>
+        <p className="text-3xl text-green-200 text-center">location 1: 295 reindeer road, pleasant, whoville 3409 </p>
         <br></br>
 
-        <p className="text-3xl text-blue-800"> location 2: 6970 Cool St, Hobbit Hole, whoville 3409 </p>
+        <p className="text-3xl text-blue-800 "> location 2: 6970 Cool St, Hobbit Hole, whoville 3409 </p>
         <br></br>
 
-        <p className="text-3xl text-purple-600"> location 3: 1234 North Pole, Whoville 3409 </p>
+        <p className="text-3xl text-purple-600 text-center"> location 3: 1234 North Pole, Whoville 3409 </p>
         <br></br>
 
         <p className="text-3xl text-pink-400"> location 4: 5678 Candy Cane Lane, Whoville 3409 </p>
         <br></br>
 
-        <p className="text-3xl text-yellow-400"> location 5: 9101 Gingerbread Ave, Whoville 3409 </p>
+        <p className="text-3xl text-yellow-400 text-center"> location 5: 9101 Gingerbread Ave, Whoville 3409 </p>
         <br></br>
 
 
